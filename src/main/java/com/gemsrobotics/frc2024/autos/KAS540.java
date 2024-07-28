60e94a9f6226f66b2762c831b7c6afb54e2c2fbe
CraftyDWB <danielwb416@gmail.com>
2024-07-28 10:26:30 -0400

    KAS 543 and 540 rewrite
    contained within: shenanigans abound

--- src/main/java/com/gemsrobotics/frc2024/autos/KAS540.java
+++ src/main/java/com/gemsrobotics/frc2024/autos/KAS540.java
@@ -18,19 +18,30 @@ public class KAS540 extends SequentialCommandGroup {
 
 	public KAS540() {
 		final var drive = Swerve.getInstance();
-		final var driveTo1stShot = drive.getTrackTrajectoryCommand(AUTO_NAME + ".1", true);
-		final var driveTo2ndShot = drive.getTrackTrajectoryCommand(AUTO_NAME + ".2", false);
-		final var driveToPreload = drive.getTrackTrajectoryCommand(AUTO_NAME + ".3", false);
-		//.4 is a placeholder so the trajectory works
-		final var driveTo3rdShot = drive.getTrackTrajectoryCommand(AUTO_NAME + ".5", false);
-		final var driveToMiddle = drive.getTrackTrajectoryCommand(AUTO_NAME + ".6", false);
+		final var driveToPickup = drive.getTrackTrajectoryCommand(AUTO_NAME + " 1" + ".1", true);
+		final var driveTo1stShot = drive.getTrackTrajectoryCommand(AUTO_NAME + " 2" + ".1", true);
+		final var driveTo2ndShot = drive.getTrackTrajectoryCommand(AUTO_NAME + " 2" + ".2", false);
+		final var driveToPreload = drive.getTrackTrajectoryCommand(AUTO_NAME + " 2" + ".3", false);
+		// .4 is fake and not real to account for movement from picking up the note with the ApproachNoteCommand
+		final var driveTo3rdShot = drive.getTrackTrajectoryCommand(AUTO_NAME + " 2" + ".5", false);
+		final var driveToMiddle = drive.getTrackTrajectoryCommand(AUTO_NAME + " 2" + ".6", false);
+
+		//final var driveToPickup = drive.getTrackTrajectoryCommand(AUTO_NAME + " 1" + ".1", true);
+		//final var driveTo1stShot = drive.getTrackTrajectoryCommand(AUTO_NAME + " 2" + ".1", false);
+		//final var driveTo2ndShot = drive.getTrackTrajectoryCommand(AUTO_NAME + " 2" + ".2", false);
+		//final var driveTo3rdShot = drive.getTrackTrajectoryCommand(AUTO_NAME + " 2" + ".3", false);
+		//final var driveToMiddle = drive.getTrackTrajectoryCommand(AUTO_NAME + " 2" + ".4", false);
 
 
 		addCommands(
-				drive.resetOdometryOnTrajectory(AUTO_NAME + ".1"),
+				drive.resetOdometryOnTrajectory(AUTO_NAME + " 1" + ".1"),
 //				new SetIntakeForcedOutCommand(true),
 				new ParallelDeadlineGroup(
-						driveTo1stShot,
+						new SequentialCommandGroup(
+								driveToPickup,
+								new WaitCommand(0.1), // bouncing lol
+								driveTo1stShot
+						),
 						new SequentialCommandGroup(
 								new WaitCommand(0.25),
 								new SetWantedStateCommand(Superstructure.WantedState.INTAKING)
@@ -48,7 +59,7 @@ public class KAS540 extends SequentialCommandGroup {
 				new ParallelDeadlineGroup(
 						new SequentialCommandGroup(
 								driveToPreload, // drive most of the way to est. preload location
-								new ApproachNoteCommand(1), // use vision to find the note once nearby
+								new ApproachNoteCommand(1.5), // use vision to find the note once nearby
 								new WaitCommand(0.1), // bouncing lol
 								driveTo3rdShot
 						),

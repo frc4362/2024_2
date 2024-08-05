a35d23d447725910f0c957d89f62ff290bd122d3
CraftyDWB <danielwb416@gmail.com>
2024-08-05 19:45:11 -0400

    453 fix & created 54

--- src/main/java/com/gemsrobotics/frc2024/autos/KAS54.java
+++ src/main/java/com/gemsrobotics/frc2024/autos/KAS54.java
@@ -11,18 +11,19 @@ import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 
-public class KAS453 extends SequentialCommandGroup {
+public class KAS54 extends SequentialCommandGroup {
 
-	private static final String AUTO_NAME = "KAS 453";
+	private static final String AUTO_NAME_START = "KAS 543";
+	private static final String AUTO_NAME = "KAS 53";
 
-	public KAS453() {
+	public KAS54() {
 		final var drive = Swerve.getInstance();
-		final var driveToPickup = drive.getTrackTrajectoryCommand(AUTO_NAME + ".1", true);
-		// .2 is skipped to account for drift
-		final var driveTo1stShot = drive.getTrackTrajectoryCommand(AUTO_NAME + ".3", true);
+		final var driveToPickup = drive.getTrackTrajectoryCommand(AUTO_NAME_START + " 1" + ".1", true);
+		final var driveTo1stShot = drive.getTrackTrajectoryCommand(AUTO_NAME + ".1", false);
+		final var driveTo2ndPickup = drive.getTrackTrajectoryCommand(AUTO_NAME + ".2", false);
+//		final var driveTo3rdShot = drive.getTrackTrajectoryCommand(AUTO_NAME + ".3", false);
 		final var driveTo2ndShot = drive.getTrackTrajectoryCommand(AUTO_NAME + ".4", false);
-		final var driveTo3rdShot = drive.getTrackTrajectoryCommand(AUTO_NAME + ".5", false);
-		final var driveToMiddle = drive.getTrackTrajectoryCommand(AUTO_NAME + ".6", false);
+		final var driveToMiddle = drive.getTrackTrajectoryCommand(AUTO_NAME + ".5", false);
 
 //		final var driveToFirstShot = drive.getTrackTrajectoryCommand(AUTO_NAME + " 1" + ".1", true);
 //		final var driveToPickup = drive.getTrackTrajectoryCommand(AUTO_NAME + " 1" + ".2", false);
@@ -32,7 +33,7 @@ public class KAS453 extends SequentialCommandGroup {
 //		final var driveToMiddle = drive.getTrackTrajectoryCommand(AUTO_NAME + " 4" + ".4", false);
 
 		addCommands(
-				drive.resetOdometryOnTrajectory(AUTO_NAME + ".1"),
+				drive.resetOdometryOnTrajectory(AUTO_NAME_START + " 1" + ".1"),
 				new SetIntakeForcedOutCommand(true),
 				new ParallelDeadlineGroup(
 						new SequentialCommandGroup(
@@ -47,22 +48,26 @@ public class KAS453 extends SequentialCommandGroup {
 				),
 				new ConditionalCommand(new ShootNoteCommand(2.0, true), new WaitCommand(0.25), () -> Fintake.getInstance().isHoldingPiece()),
 				new ParallelDeadlineGroup(
-						driveTo2ndShot,
+						new SequentialCommandGroup(
+								driveTo2ndPickup,
+								new WaitCommand(0.05),
+								driveTo2ndShot
+						),
 						new SequentialCommandGroup(
 								new WaitCommand(0.25),
 								new SetWantedStateCommand(Superstructure.WantedState.INTAKING)
 						)
 				),
 				new ConditionalCommand(new ShootNoteCommand(2.0, true), new WaitCommand(0.25), () -> Fintake.getInstance().isHoldingPiece()),
-				new ParallelDeadlineGroup(
-						driveTo3rdShot,
-						new SequentialCommandGroup(
-								new WaitCommand(0.25),
-								new SetWantedStateCommand(Superstructure.WantedState.INTAKING)
-						)
-				),
+//				new ParallelDeadlineGroup(
+//						driveTo3rdShot,
+//						new SequentialCommandGroup(
+//								new WaitCommand(0.25),
+//								new SetWantedStateCommand(Superstructure.WantedState.INTAKING)
+//						)
+//				),
 				new SetIntakeForcedOutCommand(false),
-				new ConditionalCommand(new ShootNoteCommand(2.0, true), new WaitCommand(0.25), () -> Fintake.getInstance().isHoldingPiece()),
+//				new ConditionalCommand(new ShootNoteCommand(2.0, true), new WaitCommand(0.25), () -> Fintake.getInstance().isHoldingPiece()),
 				new SetWantedStateCommand(Superstructure.WantedState.IDLE),
 				driveToMiddle
 		);
